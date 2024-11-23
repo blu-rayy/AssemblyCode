9fe3ab56f562295d1cbb4deb00065daf97d39a26
Your Name <you@example.com>
2024-11-23 14:29:37 +0800

    added colors

--- midterms_calculator/midterms_resources/areacalc.asm
+++ midterms_calculator/midterms_resources/areacalc.asm
@@ -1,12 +1,15 @@
-; 32-bit NASM Area Calculator
 section .data
-    prompt db 'AREA CALCULATOR', 10
+    ; ANSI escape codes for colors
+    color_add db 0x1b, '[33m', 0  ; Yellow text
+    color_reset db 0x1b, '[0m', 0    ; Reset to default color
+    prompt db '==== AREA CALCULATOR ====', 10
     prompt_len equ $ - prompt
     
     menu db '[1] Rectangle', 10
           db '[2] Triangle', 10
-          db '[3] Square', 10, 10
-          db 'Enter Area To Calculate: ', 0
+          db '[3] Square', 10
+          db '[4] Exit', 10, 10
+          db 'Choose an Option: ', 0, 0
     menu_len equ $ - menu
 
     ; rectangle variables print
@@ -42,7 +45,6 @@ section .bss
 section .text
     global _start
 
-; Macro for printing string
 %macro PRINT 2
     mov eax, 4
     mov ebx, 1
@@ -51,7 +53,6 @@ section .text
     int 0x80
 %endmacro
 
-; Macro for reading input
 %macro READ 2
     mov eax, 3
     mov ebx, 0
@@ -61,13 +62,23 @@ section .text
 %endmacro
 
 _start:
+main_loop:
     ; Display menu
-    PRINT prompt, prompt_len
+    PRINT newline, 1
+
+    PRINT color_add, 5               ; Set color to red
+    PRINT prompt, prompt_len         ; Print the header
+    PRINT color_reset, 4             ; Reset to default color
+
     PRINT menu, menu_len
 
     ; Read menu choice
     READ input, 3
     call convert_input
+
+    ; Exit condition
+    cmp al, 4
+    je exit
 
     ; Validate choice
     cmp al, 1
@@ -84,38 +95,38 @@ _start:
     je square_area
 
 rectangle_area:
-    ; get width
+    ; Get width
     PRINT width_rectangle, width_rectangle_len
     READ input, 3
     call convert_input
     mov byte [width], al
 
-    ; get height
+    ; Get height
     PRINT height_rectangle, height_rectangle_len
     READ input, 3
     call convert_input
     mov byte [height], al
 
-    ; compute
+    ; Compute
     movzx eax, byte [width]
     movzx ebx, byte [height]
     mul ebx
     jmp display_result
 
 triangle_area:
-    ; get width/base
+    ; Get width/base
     PRINT width_triangle, width_triangle_len
     READ input, 3
     call convert_input
     mov byte [width], al
 
-    ; get height
+    ; Get height
     PRINT height_triangle, height_triangle_len
     READ input, 3
     call convert_input
     mov byte [height], al
 
-    ; compute 
+    ; Compute
     movzx eax, byte [width]
     movzx ebx, byte [height]
     mul ebx
@@ -123,13 +134,13 @@ triangle_area:
     jmp display_result
 
 square_area:
-    ; get width/side
+    ; Get width/side
     PRINT width_square, width_square_len
     READ input, 3
     call convert_input
     mov byte [width], al
 
-    ; compute
+    ; Compute
     movzx eax, al
     mul eax
     jmp display_result
@@ -158,10 +169,11 @@ display_result:
     PRINT result_msg, result_msg_len
     PRINT result, 4
     PRINT newline, 1
-    jmp exit
+    jmp main_loop  ; Return to main menu
 
 invalid_input:
     PRINT error_msg, 14
+    jmp main_loop  ; Return to main menu
 
 exit:
     mov eax, 1
@@ -174,7 +186,6 @@ int_to_str:
     mov ecx, result + 3
     mov byte [ecx + 1], 10  ; newline
 
-    ; Convert each digit 
 digit_loop:
     xor edx, edx
     div ebx
